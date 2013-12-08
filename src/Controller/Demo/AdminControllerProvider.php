6d824f032e4062947d7498592276bf879e247585
dongweiwei <dongww@gmail.com>
2013-12-08 14:00:17 +0800

    初步构建了一个AdminDemo的框架

---
+++ src/Controller/Demo/AdminControllerProvider.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by dongww.
+ * User: dongww
+ * Date: 13-12-8
+ * Time: 下午1:34
+ */
+
+namespace Controller\Demo;
+
+use Silex\Application;
+use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
+use Silex\ControllerProviderInterface;
+
+class AdminControllerProvider implements ControllerProviderInterface
+{
+    /**
+     * Returns routes to connect to the given application.
+     *
+     * @param Application $app An Application instance
+     *
+     * @return ControllerCollection A ControllerCollection instance
+     */
+    public function connect(Application $app)
+    {
+        $controllers = $app['controllers_factory'];
+
+        $controllers->get('', array($this, 'index'));
+
+        return $controllers;
+    }
+
+    public function index(Application $app, Request $request)
+    {
+        return 'hello admin ' . $request->get('name');
+    }
+
+} 
