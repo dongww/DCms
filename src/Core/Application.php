c8ce764fe8d9efc686d9056db7606c44e78430b2
dongww <dongww@gmail.com>
2013-12-30 14:26:20 +0800

    开始编写内容调取部分。

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -27,9 +27,9 @@ class Application extends baseApp
 
         if ($this['debug']) {
             if ($this['mainConfig']['display_notice']) {
-                error_reporting(E_ALL);
-            } else {
                 error_reporting(E_ALL ^ E_NOTICE);
+            } else {
+                error_reporting(E_ERROR);
             }
         } else {
             error_reporting(0);
